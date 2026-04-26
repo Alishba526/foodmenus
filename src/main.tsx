299b7091ebe37c6c13f378c160e136020a355834
Alishba <alishba@example.com>
2026-04-26 20:49:37 +0500

    Fix: Resolve build errors by updating tsconfig and React imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProductDetail from "./pages/ProductDetail";
